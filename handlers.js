c826cd11e0892c68fff9c65afdae50067d48803e
FomasTreeman <tom@team-freeman.com>
2023-03-06 13:12:16 +0000

    connecting modules

--- handlers.js
+++ handlers.js
@@ -1,20 +1,26 @@
-// Submit button
-submitButton.addEventListener("click", submitNewToDo)
+import { generateTagNumber } from "./script";
 
 // Submit input values to an object template
+// ! if we use the same button for showingthe form and submiting the form
+// ! then we need a condition to check if the form is visible or not
 
-function submitNewToDo() {
+export function submitNewToDo() {
+  // ! if (documnet.form.classlist.includes(visible) submitNewToDo())
   const textInput = document.getElementById("textInput");
   const categoryInput = document.getElementById("categoryInput");
   const dateInput = document.getElementById("dateInput");
   const timeInput = document.getElementById("timeInput");
   //also create variable for recurring choices
-  
+
   const newToDo = Object.create(toDoTemplate);
   newToDo.text = textInput.value;
   newToDo.category = categoryInput.value;
   newToDo.date = dateInput.value;
   newToDo.time = timeInput.value;
   newToDo.tag = generateTagNumber();
-  return newToDo
+  return newToDo;
+}
+
+export function removeTodo(event) {
+  event.target.parentElement.remove();
 }
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-import {} from "./handlers";
-import {} from "./renderers";
-
 // Sure there is a way to do this without declaring a global variable but can't think of it right now! This is to keep track of To Dos
+import { submitNewToDo, removeTodo } from "handlers";
+
+document.querySelector("#addButton").addEventListener("click", submitNewToDo);
+document.querySelector("#bin").addEventListener("click", removeTodo);
 
 let tagNumber = 0;
 
-function generateTagNumber() {
+export function generateTagNumber() {
   tagNumber++;
   return tagNumber;
 }
